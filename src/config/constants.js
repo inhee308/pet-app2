0fe91127795a34e3c718f1d08dfad7e683f94712
inhee <inhee308@gmail.com>
2024-10-31 17:57:35 +0900

    API주소변경

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,1 +1,3 @@
-export const API_URL="http://localhost:8080";
+export const API_URL=process.env.NODE_ENV==='production'?
+"https://port-0-pet-server-m2x0wuv26df09e62.sel4.cloudtype.app"
+:"http://localhost:8080";
