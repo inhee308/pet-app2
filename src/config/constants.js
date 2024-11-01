4477623d2f1535097f1c96c9841ecdc25eeb5859
inhee <inhee308@gmail.com>
2024-11-01 17:45:31 +0900

    수정

--- src/config/constants.js
+++ src/config/constants.js
@@ -1,3 +1,3 @@
 export const API_URL=process.env.NODE_ENV==='production'?
-"https://port-0-pet-server-m2x0wuv26df09e62.sel4.cloudtype.app"
+"https://port-0-pet-server-m2yhk2ox92133743.sel4.cloudtype.app"
 :"http://localhost:8080";
